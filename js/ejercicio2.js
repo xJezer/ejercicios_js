9843b35419cb506fc8b198d352f80c048f1c5593
Jezer <sanchez.trigueros2027@hilasal.superate.org.sv>
2025-07-23 16:07:37 -0600

    ejercicos subidos

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,16 +1,16 @@
 const ramBer = Math.floor(Math.random() * 101); 
 
-let variable = ("< 10", "< 50", "> 75")
+let variable = ["< 10", "< 50", "> 75"]
 
-condition = variable[Math.floor(Math.random() * 3)]
+let condition = variable[Math.floor(Math.random() * variable.length)];
 
-console.log("let's try your luck, your number needs to be" + variable[condition] + " to win");
+console.log("let's try your luck, your number needs to be " + condition + " to win");
 
-if (ramBer < 10 && condition === 0) {
+if (ramBer < 10 && condition === "< 10") {
     console.log("You win! Your number is less than 10.");
-}else if (ramBer < 50 && condition === 1) {
+}else if (ramBer < 50 && condition === "< 50") {
     console.log("You win! Your number is less than 50.");
-}else if (ramBer > 75 && condition === 2) {
+}else if (ramBer > 75 && condition === "> 75") {
     console.log("You win! Your number is greater than 75.");
 }else {
     console.log("You lose! Your number does not meet the condition.");
